ce032fdd39a23f79418e64aa8f4a8f632d984fc8
guitischer <guitischer@hotmail.com.br>
2022-11-15 23:17:21 -0300

    refactor components

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,61 +1,44 @@
-import { Container, Board, OrdersContainer} from './styles';
+import { Order } from '../../types/Order';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container} from './styles';
+
+const orders: Order[] = [
+  {
+    '_id': '6372eee3af36f616f0dde306',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1668475466939-quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 2,
+        '_id': '6372eee3af36f616f0dde307'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
     <Container>
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 Itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard
+        icon="🕑"
+        title="Fila de espera"
+        orders={orders}
+      />
+      <OrdersBoard
+        icon="👨‍🍳"
+        title="Em preparação"
+        orders={[]}
+      />
+      <OrdersBoard
+        icon="✅"
+        title="Pronto!"
+        orders={[]}
+      />
     </Container>
   );
 }
